2d1cffa9ca35ef9efa9a0f9e08df72b634f6e2b6
ilias-palai <ilias@onebrandstudio.gr>
2022-11-02 13:48:38 +0200

    mac commit

--- portfolio/deploy.sh
+++ portfolio/deploy.sh
@@ -1,14 +1,11 @@
-#!usr/bin/env #!/bin/sh
-
-set -e
-
-npm run build
-
+#!/bin/sh
+# echo "$(npm run build)"
+set - e
+# echo "$(npm run build)"
+echo "$PWD"
 cd dist
-
 git init
 git add -A
 git commit -m 'New Deployment'
 git push -f git@github.com:LiasLiatoras/portfolio.git master:gh-pages
-
 cd -
--- portfolio/src/components/HelloWorld.vue
+++ portfolio/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>TEST gfdgdgdfgdgfdf</h1>
+    <h1>TEST GITHUB PAGE </h1>
   </div>
 </template>
 
